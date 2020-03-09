b5e21aa4b1d2e90436b01cd8e5ab93f92ba32cf5
qiunanya <1766226354@qq.com>
2020-03-09 13:16:50 +0800

    dart变量声明方式

---
+++ 1.定义变量.dart
@@ -0,0 +1,27 @@
+/**
+ * dart 是强类型的语言：变量有自己的类型
+ * dart 变量声明方式
+ * 明确声明： 类型 变量名称=赋值
+ * 类型推导：var /const/final/dynamic
+ * 
+ */
+main(List<String> args) {
+  //1.明确的声明（制定类型）
+  String name = "hhh";
+  int age = 18;
+  double height = 1.99;
+
+  const msg = "msg";
+  final msg2 = "final";
+  //const必须直接复制常量；final 可以通过运行时赋值  
+
+  //dynamic 动态
+  dynamic str = "abc";
+  str = 456;
+  print(str.runtimeType);
+  
+  print("${name} ${age} ${height} ${str}");
+
+  print(name.runtimeType);
+
+}
---
+++ hello.dart
@@ -0,0 +1,13 @@
+/**
+ * dart的入口函数时mian
+ * 打印采用print
+ */
+// main(List<String> args) {
+//   print('hello dart');
+// }
+
+//2.dart完整函数
+void main(List<String> args) {
+  print("hello fultter");
+  print(args);
+}
